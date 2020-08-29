6a31fbea19df7fc06cb79891c2fa1f8a044a26d3
Asset Ryskul <asset89@gmail.com>
2020-08-29 17:49:32 +0600

    project setups

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Asset Ryskul on 8/29/20.
+//  Copyright © 2020 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
